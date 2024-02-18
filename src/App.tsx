ab1f87533ca0f900c32113a45dea8840214c6a18
Aman Mavai <amanmavai@gmail.com>
2024-02-18 13:12:10 +0530

    Update heading color in Home component

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function NoMatch() {
 function Home() {
   return (
     <div className="mt-8 flex flex-col items-center gap-4 ">
-      <h2 className="text-3xl text-cyan-500">Demo Components</h2>
+      <h2 className="text-3xl text-primary">Demo Components</h2>
       {/* Placeholders for components */}
       <div className="w-96 border border-gray-200 p-4 text-justify">
         <ComboboxWrapper />
